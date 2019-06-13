4705b889d1d105fbb9fdc7fc70fa8a4e63fcbdc5
Yaroslav Sklabinkyi <yaroslav.sklabinskyi@auto1.com>
2019-06-13 11:46:45 +0200

    add file headers

--- src/AbstractParts/Patch/AbstractPatchCollection.php
+++ src/AbstractParts/Patch/AbstractPatchCollection.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the auto1-oss/service-api-request package.
+ *
+ * (c) AUTO1 Group GmbH https://www.auto1-group.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Auto1\ServiceAPIRequest\AbstractParts\Patch;
 
 use Auto1\ServiceAPIRequest\ServiceRequestInterface;
