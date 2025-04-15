84016705c2e7710ed010cc680b8079c8adb6a1be
sun <alex.sun@auto1.com>
2025-04-15 16:01:41 +0200

    Added updated license headers with proper repository name and LICENSE.md

--- src/AbstractParts/Patch/AbstractPatchCollection.php
+++ src/AbstractParts/Patch/AbstractPatchCollection.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+* This file is part of the auto1-oss/service-api-request.
+*
+* (c) AUTO1 Group SE https://www.auto1-group.com
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
 namespace Auto1\ServiceAPIRequest\AbstractParts\Patch;
 
 use Auto1\ServiceAPIRequest\ServiceRequestInterface;
